aa007557e9ba8d6e2bfed452eb9b1f2b39bf87be
Taylor Lodge <taylor@boost.co.nz>
2015-01-06 11:23:31 +1300

    Use HTML.fragment and correctly scope xpath searches

--- lib/hltb/parser.rb
+++ lib/hltb/parser.rb
@@ -3,7 +3,7 @@ require 'hltb/game'
 module HLTB
   class Parser
     def initialize(search_result)
-      @parsed_results = Nokogiri::HTML(search_result)
+      @parsed_results = Nokogiri::HTML.fragment(search_result)
       raise ArgumentError, 'search_result is nil' unless search_result.present?
       raise ArgumentError, 'Invalid HTML for search_result' unless @parsed_results.css('.gamelist_list').present?
     end
@@ -19,10 +19,10 @@ module HLTB
     end
 
     def parse_game(game_node) 
-      header_node = game_node.xpath('//h3/a')
+      header_node = game_node.xpath('.//h3/a')
       title = header_node.attribute('title').value
       relative_url = header_node.attribute('href').value
-      times = game_node.xpath('//div/div/div/div')
+      times = game_node.xpath('.//div/div/div')
       main = parse_time(times[1])
       main_with_extras = parse_time(times[3])
       completionist = parse_time(times[5])
