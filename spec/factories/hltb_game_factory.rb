7559c6beb45cc8c36637c7370da89a03464b1268
Taylor Lodge <taylor@boost.co.nz>
2015-01-06 11:06:11 +1300

    New factories for metal gear DLC

--- spec/factories/hltb_game_factory.rb
+++ spec/factories/hltb_game_factory.rb
@@ -20,6 +20,22 @@ FactoryGirl.define do
     main_with_extras_length 9.5
     completionist_length 25
   end
+  
+  factory :metal_gear_blade_wolf, parent: :hltb_game do
+    name "Metal Gear Rising: Revengeance - Blade Wolf DLC"
+    url "http://howlongtobeat.com/game.php?id=5906"
+    main_length 1
+    main_with_extras_length 1
+    completionist_length 0
+  end
+
+  factory :metal_gear_jetstream, parent: :hltb_game do
+    name "Metal Gear Rising: Revengeance - Jetstream DLC"
+    url "http://howlongtobeat.com/game.php?id=5907"
+    main_length 1
+    main_with_extras_length 2
+    completionist_length 0
+  end
 
   factory :braid, parent: :hltb_game do
     name "Braid"
