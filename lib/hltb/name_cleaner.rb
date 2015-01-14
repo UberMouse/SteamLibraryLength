6d921b8c0096713521da2ccc4a6ef0a868709f69
Taylor Lodge <taylor@boost.co.nz>
2015-01-15 11:06:58 +1300

    Change subs to be case insensitive

--- lib/hltb/name_cleaner.rb
+++ lib/hltb/name_cleaner.rb
@@ -7,12 +7,12 @@ module HLTB
     end
 
     def fix_goty_edition(game_name)
-      game_name.sub('GOTY Edition', '')
-               .sub('Game of the Year Edition', '')
+      game_name.sub(/GOTY Edition/i, '')
+               .sub(/Game of the Year Edition/i, '')
     end
 
     def fix_steam_edition(game_name)
-      game_name.sub('Steam Edition', '')
+      game_name.sub(/Steam Edition/i, '')
     end
   end
 end
