1ad3febc66904cfe30fc491636a6caec2ac4117a
Taylor Lodge <taylor@boost.co.nz>
2015-01-15 10:32:31 +1300

    Remove 'Steam Edition' from games

--- lib/hltb/name_cleaner.rb
+++ lib/hltb/name_cleaner.rb
@@ -1,14 +1,18 @@
 module HLTB
   class NameCleaner
     def call(game_name)
-      fixer_functions = [:goty_edition]
-      fixer_functions.map{|ff_name| method("fix_#{ff_name}".to_s)}
+      fixer_functions = self.methods.grep(/fix/)
+      fixer_functions.map{|ff_name| method(ff_name)}
                      .reduce(game_name) {|game_name, fixer| fixer.call(game_name)}.strip
     end
 
     def fix_goty_edition(game_name)
       game_name.sub('GOTY Edition', '')
                .sub('Game of the Year Edition', '')
+    end
+
+    def fix_steam_edition(game_name)
+      game_name.sub('Steam Edition', '')
     end
   end
 end
